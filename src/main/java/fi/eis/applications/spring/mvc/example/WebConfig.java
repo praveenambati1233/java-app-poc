6d1e43d4fa2adf0e926044af99d87daec01d538b
praveenambati1233 <praveenambati1233@gmail.com>
2024-11-26 09:48:03 +0530

    remove test folder

--- src/main/java/fi/eis/applications/spring/mvc/example/WebConfig.java
+++ src/main/java/fi/eis/applications/spring/mvc/example/WebConfig.java
@@ -15,7 +15,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@Import({HomeConfig.class, CounterConfig.class})
+@Import({HomeConfig.class})
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
